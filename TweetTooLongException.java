3dbda5105d2936bb6209297847e24a2a6b8a19a7
Omer Ahmed <omer1@ualberta.ca>
2017-02-02 16:48:21 -0700

    Filled out TweetTooLongException class details
    That class now has constructors and is no longer empty. I think I know why this class was giving me errors before...

--- TweetTooLongException.java
+++ TweetTooLongException.java
@@ -2,8 +2,23 @@ package ca.ualberta.cs.lonelytwitter;
 
 /**
  * Created by omer1 on 1/17/17.
+ * Class contents copied (with permission) from https://github.com/bfleyshe/lonelyTwitter as a result of missing that part of the lab session
+ * 2017-01-24 18:19
  */
-
 public class TweetTooLongException extends Exception {
+    /**
+     * Instantiates a new Tweet too long exception.
+     */
+    public TweetTooLongException(){
 
+    }
+
+    /**
+     * Instantiates a new Tweet too long exception.
+     *
+     * @param detailMessage the detail message
+     */
+    public TweetTooLongException(String detailMessage) {
+        super(detailMessage);
+    }
 }
